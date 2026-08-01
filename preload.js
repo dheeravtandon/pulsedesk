2fcf3b40fc10e653b0757a36fded3e9a8a4c1038
Manu Tandan <tdheerav@gmail.com>
2026-08-01 08:06:26 +0530

    feat: context-isolated preload bridge

---
+++ preload.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('pulse', {
+  get: () => ipcRenderer.invoke('data:get'),
+  refresh: () => ipcRenderer.invoke('data:refresh'),
+  onUpdate: (cb) => {
+    const handler = (_e, patch) => cb(patch);
+    ipcRenderer.on('data:update', handler);
+    return () => ipcRenderer.removeListener('data:update', handler);
+  },
+  portfolio: {
+    read: () => ipcRenderer.invoke('portfolio:read'),
+    add: (h) => ipcRenderer.invoke('portfolio:add', h),
+    update: (id, patch) => ipcRenderer.invoke('portfolio:update', { id, patch }),
+    remove: (id, sellPrice) => ipcRenderer.invoke('portfolio:remove', { id, sellPrice }),
+    setBase: (c) => ipcRenderer.invoke('portfolio:base', c),
+    setCash: (a) => ipcRenderer.invoke('portfolio:cash', a),
+    import: (d) => ipcRenderer.invoke('portfolio:import', d)
+  },
+  settings: {
+    get: () => ipcRenderer.invoke('settings:get'),
+    set: (patch) => ipcRenderer.invoke('settings:set', patch)
+  },
+  win: {
+    minimize: () => ipcRenderer.invoke('win:minimize'),
+    hide: () => ipcRenderer.invoke('win:hide'),
+    quit: () => ipcRenderer.invoke('win:quit')
+  },
+  openExternal: (url) => ipcRenderer.invoke('shell:open', url),
+  lookup: (symbol) => ipcRenderer.invoke('search:quote', symbol)
+});
